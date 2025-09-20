2294ee4e4b0004bdb723d728b61a78b3fb9238ff
maman <maman@automatalabs.co>
2025-09-21 01:25:27 +0700

    updated
    Signed-off-by: maman <maman@automatalabs.co>

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-      </main>
-    </div>
-  );
+	return (
+		<body class="bg-gray-900 text-white flex items-center justify-center min-h-screen">
+			<div class="text-center px-4">
+				<h1 class="text-6xl font-extrabold mb-4">Coming Soon</h1>
+				<p class="text-lg text-gray-400">Website ini sedang dalam pengembangan. Nantikan update selanjutnya!</p>
+			</div>
+		</body>
+	);
 }
