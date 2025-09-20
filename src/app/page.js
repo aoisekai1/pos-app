a52d19556448a3c8e8c7e65a5bab394b4849ba96
maman <maman@automatalabs.co>
2025-09-21 01:29:51 +0700

    updated
    Signed-off-by: maman <maman@automatalabs.co>

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ export default function Home() {
 		<body class="bg-gray-900 text-white flex items-center justify-center min-h-screen">
 			<div class="text-center px-4">
 				<h1 class="text-6xl font-extrabold mb-4">Coming Soon</h1>
-				<p class="text-lg text-gray-400">Website ini sedang dalam pengembangan. Nantikan update selanjutnya!</p>
+				<p class="text-lg text-gray-400">
+					Aplikasi POS gratis ini masih dalam tahap pembuatan dan pengembangan. 
+					Kami berkomitmen untuk menyediakan aplikasi tanpa biaya pembuatan. 
+					Mohon bersabar dan tunggu update selanjutnya!
+				</p>
 			</div>
 		</body>
 	);
