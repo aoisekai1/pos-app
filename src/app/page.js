9bb3acc578af53c76f18f906168ccc9272f225e0
maman <maman@automatalabs.co>
2025-09-21 01:31:56 +0700

    updated
    Signed-off-by: maman <maman@automatalabs.co>

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<body class="bg-gray-900 text-white flex items-center justify-center min-h-screen">
+		<div class="bg-gray-900 text-white flex items-center justify-center min-h-screen">
 			<div class="text-center px-4">
 				<h1 class="text-6xl font-extrabold mb-4">Coming Soon</h1>
 				<p class="text-lg text-gray-400">
@@ -9,6 +9,6 @@ export default function Home() {
 					Mohon bersabar dan tunggu update selanjutnya!
 				</p>
 			</div>
-		</body>
+		</div>
 	);
 }
